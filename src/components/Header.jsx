8391980aae862497f9ff8d73057cbb1db820b933
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-25 13:45:19 -0700

    added style to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <img src="https://live.staticflickr.com/65535/50465001743_70d1d25203_o.png" alt="pokemon"/>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Login</li>
-            </ul>
+            <img className="banner" src="https://live.staticflickr.com/65535/50465001743_70d1d25203_o.png" alt="pokemon"/>
         </header>
     )
 }
