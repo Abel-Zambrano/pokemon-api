ab43beebbc0d0f7bc120e00e9c6cd7c98011e1e3
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-27 20:19:40 -0700

    update

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -5,25 +5,33 @@ const Pokemon = () => {
     const [ avatar, setAvatar ] = useState({})
 
     useEffect(() => {
-        axios
-            .get('https://pokeapi.co/api/v2/pokemon/haunter')
-            .then(res => {
-                setAvatar(res.data)
-                
-            })
-            .catch(err => {
-                console.log(err);
-                
-            })
+        async function fetchData() {
+            const request = await axios.get('https://pokeapi.co/api/v2/pokemon/haunter')
+            setAvatar(request.data)
+            return request;
+            
+        }
+        fetchData();
     }, []);
+
+console.log(avatar.species);
 
     return (
         <div className="card">
             <h1 className="poke-name">{avatar.name}</h1>
-            {console.log(avatar)}
         </div>
     )
 }
     
-
 export default Pokemon;
+
+    // axios
+        //     .get('https://pokeapi.co/api/v2/pokemon/haunter')
+        //     .then(res => {
+        //         setAvatar(res.data)
+                
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+                
+        //     })
