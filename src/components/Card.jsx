b268595d4c63ad266c1f4df9dd2e7193605d5fee
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-28 20:22:07 -0700

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,39 +2,33 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Card = () => {
-    const [ pokeData, setPokeData ] = useState({
-        name: '',
-        pic: '',
-        type: '',
-        hp: 0,
-        attack: 0
-
-    })
+    const [ pokeData, setPokeData ] = useState([]);
 
     useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon/haunter')
+        axios
+            .get('https://pokeapi.co/api/v2/pokemon/151')
             .then(res => {
-                console.log(res.data);
-                setPokeData({
-                    name: res.data.name,
-                    pic: res.data.sprites.other.dream_world.front_default,
-                    type: 'Loading',
-                    hp: '',
-                    attack: 0
-                })
+                setPokeData(res.data);
             })
-            
-            
-    }, [setPokeData]);
+            .catch(err => {
+                console.log(err);
+                
+            })
+    }, []);
 
 console.log(pokeData);
- 
-    
+
+    const pokeType = (pokeData && pokeData.types) && <h5>Type: {pokeData.types[0].type.name}</h5>
+    const pokeHp = (pokeData && pokeData.stats) && <h5>HP: {pokeData.stats[0].base_stat} </h5>
+    const pokeAbility = (pokeData && pokeData.abilities) && <h5>Ability: {pokeData.abilities[0].ability.name} </h5>
+
     return (
         <div className="card">
-            <h1 className="poke-name">{pokeData.name}</h1>
-            <img src={pokeData.pic} alt="pokemon"/>
-            <p>Type: {pokeData.type}</p>
+            <h1 className="poke-name">{pokeData.species?.name}</h1>
+            <img src={pokeData.sprites?.front_default} alt=""/>
+            {pokeHp}
+            {pokeAbility}
+            {pokeType}
         </div>
     )
 }
